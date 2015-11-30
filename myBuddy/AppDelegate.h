24bb3733b7d433afeec66a99f4ed71ed2b89c874
Venkata <tejanvm92@gmail.com>
2015-11-30 09:01:55 -0500

    project template added to bit bucket

---
+++ myBuddy/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  myBuddy
+//
+//  Created by Venkata Maniteja on 2015-11-30.
+//  Copyright © 2015 Venkata Maniteja. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ myBuddy/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  myBuddy
+//
+//  Created by Venkata Maniteja on 2015-11-30.
+//  Copyright © 2015 Venkata Maniteja. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
