0e22a6cdb824ff32e5055fe79d72e2c5ab56144f
Dhrupa <dhrumaamin@gmail.com>
2015-12-02 00:36:02 -0500

    Added Camera view controller and list view controller using sqlite 3 currently camera view is a initial view controller so change that to login view controller

---
+++ myBuddy/CameraViewController.h
@@ -0,0 +1,13 @@
+//
+//  CameraViewController.h
+//  myBuddy
+//
+//  Created by Dhrupa on 2015-12-01.
+//  Copyright © 2015 Venkata Maniteja. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CameraViewController : UIViewController
+
+@end
