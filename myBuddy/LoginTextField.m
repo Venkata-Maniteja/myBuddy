1fce84c936b6d167dfd369b30ca19a1752c9bf54
Dhrupa <dhrumaamin@gmail.com>
2015-12-01 19:07:06 -0500

    i added a comment

--- myBuddy/LoginTextField.m
+++ myBuddy/LoginTextField.m
@@ -10,6 +10,8 @@
 
 @implementation LoginTextField
 
+//added commwent
+
 /*
 // Only override drawRect: if you perform custom drawing.
 // An empty implementation adversely affects performance during animation.
